88ce14e03a43816480bf19d784d8f3f22ec60338
Chikipooki <AltarNatsev@yandex.ru>
2023-03-01 14:29:35 +0700

    slider animation added

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -25,12 +25,44 @@ $(function() {
     $("[data-scroll]").on("click", function(event) {
         event.preventDefault();
 
-        var blockId = $(this).data('scroll'),
+        var $this = $(this),
+            blockId = $this.data('scroll'),
             blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
 
         $("html, body").animate({
             scrollTop: blockOffset
         }, 500);
     });
 
+
+    /*Menu nav toggle */
+    $("#nav-toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+    /*collapse */
+
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        $(blockId).slideToggle();
+        $(this).toggleClass("active");
+    });
+
+    /*slider */
+    $("[data-slider]").slick({
+        infinite: true,
+        fade: false,
+        slidesToSHow: 1,
+        slideToScroll: 1
+    });
 });
